5dfad1d6553068dcb27672887ff7acb271eb5a29
Syamantak <Syamantak1305@gmail.com>
2025-01-17 23:56:02 +0530

    CreatePost.jsx page was modified.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -5,8 +5,51 @@ import {preview} from'../assets'
 import { getRandomPrompt } from '../utils';
 import {FormField, Loader} from '../components'
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setform] = useState({
+    name: '',
+    prompt: '',
+    photo:'',
+  });
+  const [generatingImg, setgeneratingImg] = useState(false);
+  const [loading, setloading] = useState(false);
+
+  const handleSubmit =()=>{}
+
+  const handleChange=(e)=>{}
+
+  const handleSupriseMe=()=>{}
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text -[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative and visually stunning images generated from DALL -E AI and share with community</p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName="Your Name"
+            type='text'
+            name='name'
+            placeholder='Enter your name'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type='text'
+            name='prompt'
+            placeholder='Enter your prompt'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSupriseMe
+            handleSupriseMe={handleSupriseMe}
+          />
+        </div>
+
+      </form>
+    </section>
   )
 }
 
