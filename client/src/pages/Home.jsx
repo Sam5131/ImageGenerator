e3ed34c5061d12ce6f034a087a3b6ee0239b594e
Syamantak <Syamantak1305@gmail.com>
2025-01-19 15:06:38 +0530

    Card.jsx modified to have card representation in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,6 +15,30 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const[allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('')
+
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/vv1/post',{
+          method: 'GET',
+          headers: {
+            'Content-Type':'application/json'
+          },
+        })
+        if(response.ok){
+          const result = await response.json();
+          setAllPosts(result.data.reverse)
+        }
+      } catch (error) {
+        alert(error)
+      }finally{
+        setLoading(false)
+      }
+    }
+    fetchPosts();
+  },[])
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
